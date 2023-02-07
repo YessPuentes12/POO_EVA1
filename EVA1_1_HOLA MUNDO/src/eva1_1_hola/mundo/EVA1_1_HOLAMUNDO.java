9b59c9e3f3df29b9d8b1769fd715549c111df0a5
YessPuentes12 <106281825+YessPuentes12@users.noreply.github.com>
2023-02-07 09:39:14 -0600

    Add files via upload

---
+++ EVA1_1_HOLA MUNDO/src/eva1_1_hola/mundo/EVA1_1_HOLAMUNDO.java
@@ -0,0 +1,16 @@
+/*
+smn
+ */
+package eva1_1_hola.mundo;
+
+
+public class EVA1_1_HOLAMUNDO {
+ 
+    
+    public static void main(String[] args) {
+        System.out.println("Hola mundo.");
+  }
+    
+  
+  
+}
